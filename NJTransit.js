47d5c2c7dfb907f9dc2c021694fba7be33851492
Shadowclysm <kang@45.79.145.100>
2020-01-15 03:13:36 +0000

    init

---
+++ NJTransit.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name        NJTranshit Filter
+// @namespace   Violentmonkey Scripts
+// @match       https://njtranshit.com/schedule/9/northeast-corridor*
+// @grant       none
+// @version     1.0
+// @author      https://github.com/Shadowclysm
+// @updateURL   https://raw.githubusercontent.com/Shadowclysm/UserScripts/master/NJTransit.js
+// ==/UserScript==
+
+var filter = document.createElement('input');
+filter.value = 'NEW BRUNSWICK,NEWARK PENN STATION';
+document.body.prepend(filter);
+filter.style.width = '50%';
+
+var button = document.createElement('button');
+button.innerHTML = 'hide';
+document.body.prepend(button);
+
+
+button.onclick = function(){
+  var list = filter.value.split(',');
+  list.push('');
+
+  document.querySelectorAll('tbody > tr > th').forEach(function(e){
+    e.parentElement.style.display = '';
+  });
+
+  if(button.innerHTML=='show'){
+    button.innerHTML='filter';
+  }else{
+    button.innerHTML = 'show';
+    document.querySelectorAll('tbody > tr > th').forEach(function(e){
+      if(!list.includes(e.innerHTML)) e.parentElement.style.display = 'none';
+    });
+  }
+}
+
+button.click();
